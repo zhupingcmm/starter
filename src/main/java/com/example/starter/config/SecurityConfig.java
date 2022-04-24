b6e2467db94eef540fb4e3d86151550584237016
pingzhu <ping.zhu@microfocus.com>
2022-04-24 09:28:05 +0800

    mutiple encript password

--- src/main/java/com/example/starter/config/SecurityConfig.java
+++ src/main/java/com/example/starter/config/SecurityConfig.java
@@ -10,14 +10,14 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -39,12 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers("/api/**").hasRole("USER")
                         .anyRequest().authenticated())
                 .addFilterAt(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .addFilterAt(new RequestResponseLoggingFilter(objectMapper), BasicAuthenticationFilter.class)
                 .csrf(csrf->csrf.disable());
-//                .formLogin(form-> form.successHandler(jsonAuthenticateSuccessHandler())
-//                        .failureHandler(authenticationFailureHandler()))
-//                .httpBasic(Customizer.withDefaults())
-//                .logout(logout-> logout.logoutSuccessHandler())
-//                .csrf(AbstractHttpConfigurer::disable);
     }
 
     private RestAuthenticationFilter restAuthenticationFilter() throws Exception {
@@ -58,7 +54,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AuthenticationFailureHandler authenticationFailureHandler() {
         return (req, res, exception) -> {
-            val objectMapper = new ObjectMapper();
             val errorData = new HashMap<String, String>();
             errorData.put("title", "failed");
             errorData.put("details", exception.getMessage());
@@ -71,7 +66,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AuthenticationSuccessHandler jsonAuthenticateSuccessHandler() {
         return (req, res, auth) -> {
-            val objectMapper = new ObjectMapper();
             val successData = new HashMap<String, String>();
             successData.put("title", "success");
             res.setStatus(HttpStatus.OK.value());
@@ -84,13 +78,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password(passwordEncoder().encode("12345678"))
-                .roles("USER", "ADMIN");
+                .password("{bcrypt}$2a$10$.OM2PPj0LHEnk2iK69JcvehDDoVY03W3IT2DyMXqzHAM6wmwSCwPy")
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("zhangsan")
+                .password("{SHA-1}{FFdDqBT2ePYO5lsm5Uj04mYBO38ZHV3N5a9khhCBSqU=}c952d833b2ec2c9f038c631961e7bd3bdc453de7")
+                .roles("USER");
     }
 
     @Bean
     PasswordEncoder passwordEncoder () {
-        return new BCryptPasswordEncoder();
+        val idForDefault = "bcrypt";
+        val encoders = new HashMap<String, PasswordEncoder>();
+        encoders.put(idForDefault, new BCryptPasswordEncoder());
+        encoders.put("SHA-1", new MessageDigestPasswordEncoder("SHA-1"));
+        return new DelegatingPasswordEncoder(idForDefault, encoders);
     }
 
     @Override
